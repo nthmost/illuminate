25fc360380fd4cd7cc6cb5a47e879c86ef5683aa
nthmost <naomi@nthmost.com>
2013-10-16 17:36:48 -0700

    completed command line tool for generating timeseries snapshots of in-progress sequencing runs

--- seqrun_spider.py
+++ seqrun_spider.py
@@ -31,25 +31,26 @@ __doc__="""Seqrun Spider
 This script acquires the minimum viable datasets necessary to extract metrics from
 MiSeq and HiSeq runs (namely, the XML files and the InterOp directory).
 
-The first argument must be the root directory to source runs from.
+The one required argument is the root directory to source runs from.  
 
-The second argument will be the run directories themselves.
+The second argument can be an unlimited space-separated list of specific run directories.
 
-Supply as many subdirectories as you like. 
+If you want to copy all of the runs from the root directory, omit the <rundirs> argument.
 
-"*" is a valid argument (retrieve all subdirectories of the root location).
+To generate progressive snapshots of the same run dir, enable the -t / --timestamp option.
 
 Usage:
   seqrun_spider.py [options] <rootdir> <rundirs>...
-  seqrun_spider.py [options] <rootdir> *
+  seqrun_spider.py [options] <rootdir>
 
-  -h --help                         Show this screen.
-  --version                         Show version.
-  -o --outputdir=<%(destdir)s>        Directory to store copied files.
-  -v --verbose                      Print a few more status messages to stdout.
-  -d --debug                        Include a lot more information in output (cumulative with --verbose).
-  -q --quiet                        Suppress all messages (except for fatal errors) (overrides -d and -v).
-""".format(DEFAULTS)
+  -h --help             Show this screen.
+  --version             Show version.
+  -o --outputdir=<%(destdir)s>    Directory to store copied files.
+  -t --timestamp        Puts data in subdirectory of outputdir named with current timestamp.
+  -v --verbose          Print a few more status messages to stdout.
+  -d --debug            Include a lot more information in output (cumulative with --verbose).
+  -q --quiet            Suppress all messages (except for fatal errors) (overrides -d and -v).
+""" % DEFAULTS
 
 VERBOSITY = 1
 DEBUG = False
@@ -92,20 +93,19 @@ def mark_dir(code, directory):
     do_command(make_mark)
 
 def get_MVDataset(abspath, args):
-    """   
-       * copy the InterOp subdirectory of targetdir to new directory under args['<outputdir>']
-       * use the name of the source directory to "mark" the newly created folder (see MARK_CMD).
     """
-
-    simple_timestamp = str(time.time()).split('.')[0]
-    print abspath
-    sourcedirname = os.path.split(os.path.split(abspath)[0])
-
+    Copies the InterOp subdirectory of targetdir to new directory under args['--outputdir']
+    """
+    sourcedirname = abspath.split('/')[-1]
     dmesg("DATASET: %s" % sourcedirname, 1)
 
-    destdir = os.path.join(args['<outputdir>'], sourcedirname)
+    destdir = os.path.join(args['--outputdir'], sourcedirname)
     create_dir(destdir)
-    create_dir(os.path.join(destdir, simple_timestamp))
+    
+    if args['--timestamp']:
+        simple_timestamp = str(time.time()).split('.')[0]
+        destdir = os.path.join(destdir, simple_timestamp)
+        create_dir(destdir)
 
     # copy "InterOp" directory as a whole.
     copy_interop = RCOPY_CMD % (os.path.join(abspath,"InterOp"), destdir)
@@ -129,6 +129,32 @@ def available_rundirs(targetdir):
         print "(Maybe you need to mount your data directory?)"
         sys.exit()
 
+def get_abspath(rootdir, subdir):
+    abspath = os.path.join(rootdir, subdir)
+    if os.path.exists(abspath):
+        return abspath
+    else:
+        dmesg("Skipping %s -- not a valid directory to source files from." % abspath, 1)
+
+def main(args):
+    rundirs = []
+    abspath_list = []
+    set_verbosity(args)
+    rootdir = args['<rootdir>']
+    if args['<rundirs>'] == []:
+        rundirs = os.listdir(rootdir)
+    else:
+        rundirs = args['<rundirs>']
+
+    for item in rundirs:
+        if item in IGNORE_LIST:
+            continue
+        abspath_list.append(get_abspath(rootdir, item))
+
+    for abspath in abspath_list:
+        get_MVDataset(abspath, args)
+
+
 def set_verbosity(args):
     global VERBOSITY
     global DEBUG
@@ -142,32 +168,6 @@ def set_verbosity(args):
     if args['--quiet']:
         VERBOSITY = -1
 
-
-def get_abspath(rootdir, subdir):
-    abspath = os.path.join(rootdir, subdir)
-    if os.path.exists(abspath):
-        return abspath
-    else:
-        dmesg("Skipping %s -- not a valid directory to source files from." % abspath, 1)
-
-def main(args):
-    rundirs = []
-    abspath_list = []
-    set_verbosity(args)
-
-    rootdir = args['<rootdir>']
-    if args['<rundirs>'] == ['*']:
-        rundirs = os.listdir(rootdir)
-    else:
-        rundirs = args['<rundirs>']
-
-    for item in rundirs:
-        if item in IGNORE_LIST:
-            continue
-        abspath_list.append(get_abspath(rootdir, item))
-
-    for abspath in abspath_list:
-        get_MVDataset(abspath, args)
 
 if __name__=='__main__':
     args = docopt(__doc__, version=0.1)
